8dc3b0fbd470450ecc712d29c6bfcf341a727812
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-10 04:28:35 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,6 +1,10 @@
 export default function manageTodo(state = {
   todos: [{text: 'buy grocereis'}, {text: 'watch netflix'}],
 }, action) {
+  switch(action.type){
+    case 'ADD_TODO':
+      console.log({todos: state.todos.concat(action.payload.text)})
+  }
 
   console.log("reducer received this action:", action);
 
